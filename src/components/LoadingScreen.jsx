9250848ab33dbe8046cd538b2bb960309b8819fd
RounitSrivastava <rounit.kumar2702@gmail.com>
2025-05-04 17:17:13 +0530

    message

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,23 +1,47 @@
 import React, { useEffect, useState } from "react";
 import "./LoadingScreen.css";
 
+const greetings = [
+  "Hello...",
+  "नमस्ते...",
+  "नमस्कार...",
+  "নমস্কার...",
+  "Hola...",        // Spanish
+     // French
+    "Hallo...",       // German
+    "Ciao...", 
+    "Welcome To MaDket",
+];
+
 const LoadingScreen = ({ onFinish }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
-    const timer = setTimeout(() => {
-      onFinish(); // Notify parent to hide loader
-    }, 4000); // 4 seconds total
-    return () => clearTimeout(timer);
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) =>
+        prev < greetings.length - 1 ? prev + 1 : 0
+      );
+    }, 300);
+
+    const finishTimer = setTimeout(() => {
+      onFinish();
+    }, 2400);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(finishTimer);
+    };
   }, [onFinish]);
 
   return (
     <div className="loading-screen">
       <div className="loading-container">
-        <span className="loading-text">Amplify</span>
-        <span className="loading-text">&nbsp;Ascend</span>
-        <span className="loading-text">&nbsp;Achieve</span>
+        <span className="loading-text fade-in">
+          {greetings[currentIndex]}
+        </span>
       </div>
     </div>
-      );
-    };
-    
-    export default LoadingScreen;
+  );
+};
+
+export default LoadingScreen;
